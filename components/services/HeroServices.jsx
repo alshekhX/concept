3d3c5ea07da54d9cbe6671498ac93bf6d88ce424
alshekhX <60186591+alshekhX@users.noreply.github.com>
2024-12-12 03:43:47 -0800

    fix font size

--- components/services/HeroServices.jsx
+++ components/services/HeroServices.jsx
@@ -13,9 +13,9 @@ const HeroServices = () => {
     <div className=" absolute inset-0 z-20 container mx-auto ">
   <div className=' mx-auto flex flex-col h-full  justify-center  px-4 py-10 '>
    <AnimateH1 initial={{ opacity: 0, y: 10 }}
-  whileInView={{ opacity: 1, y: 0 }}   transition={{ delay: 0,duration:.3}}  className="  z-10 md:text-6xl xl:text-8xl text-7xl mb-4 font-bold text-white">Services</AnimateH1>
+  whileInView={{ opacity: 1, y: 0 }}   transition={{ delay: 0,duration:.3}}  className="  z-10 md:text-6xl xl:text-8xl text-6xl mb-4 font-bold text-white">Services</AnimateH1>
   <AnimateH2  initial={{ opacity: 0, y: -20 }}
-  whileInView={{ opacity: 1, y: 0 }}   transition={{ delay: .3,duration:.3}}  className="  md:w-2/5 w-4/5 z-10 xl:text-3xl md:text-2xl text-2xl  text-white">We're your one-stop solution for all your MEP construction and supply chain requirements</AnimateH2>
+  whileInView={{ opacity: 1, y: 0 }}   transition={{ delay: .3,duration:.3}}  className="  md:w-2/5 w-5/6 z-10 xl:text-3xl sm:text-2xl text-xl  text-white">We're your one-stop solution for all your MEP construction and supply chain requirements</AnimateH2>
 </div>
   </div>
 <div className='relative'>
